e946101c3959e3f9260a45326da0265fd91ea8b9
zargarzadehm <moein.zargarzadeh@gmail.com>
2020-07-12 01:04:51 +0430

    fixed bug load data in dashboard

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dashboard/precache-manifest.e702798b1368c61a67eb171022e8d33c.js"
+  "/dashboard/precache-manifest.7e3d0d1af45a76e13bbe2ddca4e0a8e1.js"
 );
 
 self.addEventListener('message', (event) => {
